2e0104849efd6e965ed422c8671622c7c2a2c7f0
Md Hafizur Rahman <44112062+Md-Hafizur-Rahman@users.noreply.github.com>
2022-09-13 01:34:13 +0600

    Product model,colr,size,admin panel custom,product cat

--- apps/product/admin.py
+++ apps/product/admin.py
@@ -1,4 +1,19 @@
 from django.contrib import admin
-from apps.product.models import Category
+from apps.product.models import Category, Color,Product, Size,ProductPhoto
 # Register your models her
-admin.site.register(Category)
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ("name","photo","serial","slug",)
+    
+@admin.register(Color)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ("name",)
+@admin.register(Size)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ("name",)
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ("id","name","slug","catagory","price","colors","sizes","photo")
+@admin.register(ProductPhoto)
+class ProductPhotoAdmin(admin.ModelAdmin):
+    list_display = ("product","photo")
--- apps/product/models.py
+++ apps/product/models.py
@@ -1,3 +1,4 @@
+from itertools import product
 from django.db import models
 
 # Create your models here.
@@ -16,4 +17,51 @@ class Category(models.Model):
 
     def __str__(self):
         return self.name
+class Color(models.Model):
+    name=models.CharField(max_length=70)
+    def __str__(self):
+        return self.name
+class Size(models.Model):
+    name= models.CharField(max_length=70)
+    
+    def __str__(self):
+        return self.name
+class Product(models.Model):
+    name=models.CharField(max_length=70)
+    slug=models.SlugField(null=True, blank=True, unique=True)
+    catagory= models.ForeignKey(Category, on_delete=models.CASCADE,related_name='cat_product',)
+    color= models.ManyToManyField(Color,related_name='cat_color')
+    size= models.ManyToManyField(Size,related_name='cat_size')
+    price= models.FloatField()
+    photo=models.ImageField(upload_to='product')
+    
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name)+': '+str(self.price)
+            return super(Product, self).save(*args, **kwargs)
+        
+    def update(self, *args, **kwargs):  # new
+        if not self.slug:
+            a= slugify(self.name)
+            b='-'
+            c=str(self.price)
+            print('print id number',c)
+            d=a+b+c
+            self.slug=d
+        super().update(*args, **kwargs)
+    
+    def __str__(self):
+        return self.name
+    
+    def colors(self):
+        return ", ".join([p.name for p in self.color.all()])
+    def sizes(self):
+        return ", ".join([p.name for p in self.size.all()])
+class ProductPhoto(models.Model):
+    product=models.ForeignKey(Product, on_delete=models.CASCADE,related_name='product_photo')
+    photo=models.ImageField(upload_to='product')
+    
+    def __str__(self):
+        return self.product.name
+   
     
